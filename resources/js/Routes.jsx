388cf4bdbe41a7c419fa075d8ef6a13643a5327c
kkamara <kamaracomputers@gmail.com>
2023-10-21 16:43:16 +0100

    Add News Search Page in Reactjs app

--- resources/js/Routes.jsx
+++ resources/js/Routes.jsx
@@ -8,6 +8,8 @@ import Login from "./components/pages/auth/LoginComponent"
 import Logout from "./components/pages/auth/LogoutComponent"
 import Register from "./components/pages/auth/RegisterComponent"
 
+import NewsSearch from "./components/pages/NewsSearchComponent"
+
 import { url } from './utils/config'
 
 export default () => {
@@ -19,6 +21,8 @@ export default () => {
         <Route path={url("/user/login")} element={<Login />}/>
         <Route path={url("/user/logout")} element={<Logout />}/>
         <Route path={url("/user/register")} element={<Register />}/>
+        
+        <Route path={url("/news/search")} element={<NewsSearch />}/>
       </Routes>
     </>
   )
